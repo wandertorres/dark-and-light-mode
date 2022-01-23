51197cb6e12acae52005b65cdfedfa61fac017b3
Wander Torres <wander_torres@live.com>
2022-01-23 04:01:02 -0300

    main estructure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <p>Hello word!</p>
-    </div>
+import Button from "./components/Button";
+
+export default function App() {
+  return(
+    <>
+    <main>
+      <h1 className="title">Light Mode ON</h1>
+      <Button />
+    </main>
+
+    <footer>
+      Dark and Light Mode by Wander Torres
+    </footer>
+    </>
   );
 }
-
-export default App;
